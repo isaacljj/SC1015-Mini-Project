0497932481eb58f04866ddef97c198df26d05c0b
Sukingg <124869189+Sukingg@users.noreply.github.com>
2023-03-06 11:23:30 +0800

    Delete stroke_problem_dataset.py

--- stroke_problem_dataset.py
+++
@@ -1,7 +0,0 @@
-# Basic Libraries
-import numpy as np
-import pandas as pd
-import seaborn as sb
-import matplotlib.pyplot as plt
-# Import LinearRegression model from Scikit-Learn
-from sklearn.linear_model import LinearRegression
