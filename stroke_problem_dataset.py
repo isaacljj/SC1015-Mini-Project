c58bceb729ceb0fb9e6435fb4b1ce0afae5d95c6
Sukingg <124869189+Sukingg@users.noreply.github.com>
2023-03-06 11:21:01 +0800

    Create stroke_problem_dataset.py

---
+++ stroke_problem_dataset.py
@@ -0,0 +1,7 @@
+# Basic Libraries
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+# Import LinearRegression model from Scikit-Learn
+from sklearn.linear_model import LinearRegression
